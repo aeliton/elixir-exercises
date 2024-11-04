9f8b2815c78716d8e9a4cbd66357b182ad137077
Aeliton G. Silva <aeliton@gmail.com>
2024-11-04 22:30:35 +0000

    Add PatternMatching-3

--- 16/pattern_matching_1.exs
+++ 16/pattern_matching_1.exs
@@ -26,4 +26,20 @@ defmodule ProgramminElixirExercises do
     end
     assert [a, b, a] = [1, 2, 1]
   end
+
+  test "pattern_matching-3" do
+    a = 2
+    assert_raise MatchError, fn ->
+      [a, b, a] = [1, 2, 3]
+    end
+    assert_raise MatchError, fn ->
+      [a, b, a] = [1, 1, 2]
+    end
+    assert a = 1
+    assert_raise MatchError, fn ->
+      ^a = 2
+    end
+    assert ^a = 1
+    assert ^a = 2 - a
+  end
 end
