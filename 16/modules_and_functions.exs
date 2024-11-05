abcd98b5a39a55c5015908173db4e71890e588c3
Aeliton G. Silva <aeliton@gmail.com>
2024-11-05 22:22:19 +0000

    Add ModulesAndFunctions-3

--- 16/modules_and_functions.exs
+++ 16/modules_and_functions.exs
@@ -3,6 +3,7 @@ ExUnit.start()
 defmodule Times do
   def double(n), do: n * 2
   def triple(n), do: n * 3
+  def quadruple(n), do: n |> double |> double
 end
 
 defmodule ModulesAndFunctions do
@@ -15,4 +16,11 @@ defmodule ModulesAndFunctions do
   test "modules and functions-1" do
     assert Times.triple(4) === 12 
   end
+
+  # modules and functions-2: run in IEx, compile the file with a) iex <file>; and b) iex> c "<file"
+
+  test "modules and functions-3" do
+    assert Times.quadruple(4) === 16 
+  end
+
 end
