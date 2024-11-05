79c35602350e4b0213513708f98d490673664360
Aeliton G. Silva <aeliton@gmail.com>
2024-11-05 21:43:37 +0000

    Add Functions-2

--- 16/functions.exs
+++ 16/functions.exs
@@ -14,4 +14,17 @@ defmodule Functions do
     pair_tuple_to_list = fn {a, b} -> [a, b] end
     assert pair_tuple_to_list.({ 1234, 5678 }) === [ 1234, 5678 ]
   end
+
+  test "functions-2" do
+    f = fn
+      0, 0, _ -> "FizzBuzz"
+      0, _, _ -> "Fizz"
+      _, 0, _ -> "Buzz"
+      _, _, c -> c 
+    end
+    assert f.(0, 0, 3) == "FizzBuzz"
+    assert f.(0, 1, 3) == "Fizz"
+    assert f.(1, 0, 3) == "Buzz"
+    assert f.(1, 2, 3) == 3
+  end
 end
